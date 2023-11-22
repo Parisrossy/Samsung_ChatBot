07b3420f2b5d2a478e1152f446f05dc631a723d1
OLUWAYOMI ROSEMARY <140506724+Parisrossy@users.noreply.github.com>
2023-11-22 10:54:11 +0000

    Update Samsung_Dialog.py

--- Samsung_Dialog.py
+++ Samsung_Dialog.py
@@ -1,7 +1,7 @@
-# import nltk
+import nltk
 import streamlit as st
 import random
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity      
 import pandas as pd
 import warnings
